8e8a9abcf7c456dc88cee1bdec497b1e21a70e68
Vincent GONZALEZ <vincent.gon@orange.fr>
2017-05-09 15:02:49 +0200

    [db] Add Rooms model

---
+++ db/migrate/20170509125917_create_rooms.rb
@@ -0,0 +1,11 @@
+class CreateRooms < ActiveRecord::Migration[5.1]
+  def change
+    create_table :rooms do |t|
+      t.string :title
+      t.text :text
+      t.integer :proprietary_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509065246) do
+ActiveRecord::Schema.define(version: 20170509125917) do
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20170509065246) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_models_on_email", unique: true
     t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.integer "proprietary_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
