0b90042d50262948bb933b9c322b08acee39a7ea
Vincent GONZALEZ <vincent.gon@orange.fr>
2017-05-10 16:56:16 +0200

    [chat] Add rooms refresh after created

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,12 +2,17 @@ $(document).ready(function() {
     $("[name='js-api']").submit(function(e) {
         e.preventDefault();
         let form = $(this);
+        var callback_success = form.attr('data-success-callback');
+
         $.ajax({
             url: form.attr('action'),
             type: form.attr('method'),
             data: form.serialize(),
             success: function(response) {
                 console.log(response);
+                if (callback_success === "chat_displayRoomsList") {
+                    Chat.displayRoomsList();
+                }
             },
             error: function (xhr, status, error) {
                 console.log(status + " : " + error);
