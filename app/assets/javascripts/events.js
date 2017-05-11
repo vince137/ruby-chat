d6ee11933b8bbb3cd997abe493c4278abc7965fe
Vincent GONZALEZ <vincent.gon@orange.fr>
2017-05-11 09:30:36 +0200

    [bugfix][front] Use turbolinks method to load JS

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,8 +1,6 @@
-$(document).ready(function(){
-
+$(document).on('turbolinks:load', function() {
     // CHAT: ROOMS LIST
     if ($("div").attr('data-target', 'api-list').length) {
-        Chat.displayRoomsList();        
+        Chat.displayRoomsList();
     }
-
 });
