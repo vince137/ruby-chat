a48f7d834c8e7dfd783e9a967af5eb919a53a560
Vincent GONZALEZ <vincent.gon@orange.fr>
2017-05-15 09:24:06 +0200

    [db] Move to postegresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510092048) do
+ActiveRecord::Schema.define(version: 20170511120220) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "id_user"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
