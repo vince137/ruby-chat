47e17be72e5412dbfc67a8fad95e10d1b23fccc5
Vincent GONZALEZ <vincent.gon@orange.fr>
2017-05-16 11:22:19 +0200

    [core] Prevent subscrire spam with turbolinks

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -3,10 +3,11 @@ $(document).on('turbolinks:load', function() {
     if ($("div").attr('data-target', 'api-list').length) {
         Chat.displayRoomsList();
     }
+});
 
+$(document).ready(function(){
     // CHAT: MESSAGES SUBSCRIBE
     if ($(".chat-text").length) {
         Chat.messageSubscribe();
     }
-
 });
