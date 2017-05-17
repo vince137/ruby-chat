9255134473378e27c0f121307af1e31643e28f8d
Vincent GONZALEZ <vincent.gon@orange.fr>
2017-05-17 14:43:04 +0200

    [message] Use SSE logic

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,21 +1,10 @@
+require 'reloader/sse'
+
 class ChatController < ApplicationController
     include ActionController::Live
 
     def rooms
         @rooms = Room.all.order(created_at: :desc)
         render file: "chat/_rooms", layout: false
-    end
-
-    def subscribe
-        response.headers['Content-Type'] = 'text/event-stream'
-        sse = SSE.new(response.stream)
-        begin
-            Message.on_change do |messages|
-                sse.write({messages: messages}, event: "message")
-            end
-        rescue IOError
-        ensure
-            sse.close
-        end
     end
 end
